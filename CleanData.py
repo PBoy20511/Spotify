afdfae57fc2839e768439590a0d60d04d10cab8c
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-12-18 19:45:05 +0800

    Maintenance
    Comments Added

--- CleanData.py
+++ CleanData.py
@@ -5,8 +5,15 @@ from numpy import mean, var, median
 
 
 def clean_data(number: str) -> dict:
+    """ Create Features based on Spotify analysis data and will be stored in dictionary """
+
     tempt = {}
 
+    """
+    Loudness values are useful for comparing relative loudness of sections within tracks
+        - Get whole sections and last five sections loudness for mean and variance value, respectively
+        - Furthermore, find out does the loudness decrease (ai > ai+1) in last five sections
+    """
     loudness = data[number]["sections"]["loudness"]
     final = loudness[-5:]
     tempt["loudness_mean"] = mean(loudness)
@@ -17,10 +24,21 @@ def clean_data(number: str) -> dict:
         1 if final[i] > final[i + 1] else 0 for i in range(len(final) - 1)
     )
 
+    """
+    Indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived. 
+    This field will contain a 0 for “minor”, a 1 for “major”, or a -1 for no result
+        - Get whole sections mode mean and variance value
+    """
     mode = data[number]["sections"]["mode"]
     tempt["mode_mean"] = mean(mode)
     tempt["mode_var"] = var(mode)
 
+    """
+    The values of key ranging from 0 to 11 mapping to pitches using standard Pitch Class notation
+        - Get whole sections key for min, max, and median value
+        - Furthermore, find out does the key decrease (ai > ai+1) in last five sections and whole sections
+        - url: https://en.wikipedia.org/wiki/Pitch_class
+    """
     key = data[number]["sections"]["key"]
     final = key[-5:]
     tempt["key_min"] = min(key)
@@ -33,11 +51,21 @@ def clean_data(number: str) -> dict:
         1 if final[i] > final[i + 1] else 0 for i in range(len(final) - 1)
     )
 
+    """
+    The time signature (meter) is a notational convention to specify how many beats are in each bar (or measure)
+    The time signature ranges from 3 to 7 indicating time signatures of “3/4”, to “7/4”
+        - Get whole sections time_signature mean and variance value and finding is mode=3 in this song
+    """
     time_signature = data[number]["sections"]["time_signature"]
     tempt["time_signature_mean"] = mean(time_signature)
     tempt["time_signature_var"] = var(time_signature)
     tempt["time_signature_has_3"] = 3 in time_signature
 
+    """
+    In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.
+        - Get whole sections and last five sections tempo for mean and variance value, respectively
+        - Furthermore, find out does the tempo decrease (ai > ai+1) in last five sections and whole sections
+    """
     tempo = data[number]["sections"]["tempo"]
     final = tempo[-5:]
     tempt["tempo_mean"] = mean(tempo)
@@ -51,6 +79,12 @@ def clean_data(number: str) -> dict:
         1 if final[i] > final[i + 1] else 0 for i in range(len(final) - 1)
     )
 
+    """
+    The segment-relative offset of the segment peak loudness in seconds
+    Can be used to describe the “attack” of the segment
+        - Get whole sections loudness_max_time min, max, mean, and variance value
+        - Furthermore, calculate the squared sum of loudness_max_time
+    """
     loudness_max_time = data[number]["segments"]["loudness_max_time"]
     tempt["loudness_maxTime_min"] = min(loudness_max_time)
     tempt["loudness_maxTime_max"] = max(loudness_max_time)
@@ -58,6 +92,16 @@ def clean_data(number: str) -> dict:
     tempt["loudness_maxTime_var"] = var(loudness_max_time)
     tempt["loudness_maxTime_square"] = sum((pow(i, 2) for i in loudness_max_time))
 
+    """
+    For completeness however, the first dimension represents the average loudness of the segment; 
+    second emphasizes brightness; third is more closely correlated to the flatness of a sound; 
+    fourth to sounds with a stronger attack; etc. See an image below representing the 12 basis functions.
+    The actual timbre of the segment is best described as a "linear combination" of these 12 basis functions 
+    weighted by the coefficient values: timbre = c1 x b1 + c2 x b2 + … + c12 x b12, where c1 to c12 
+    represent the 12 coefficients and b1 to b12 the 12 basis functions as displayed below. 
+    Timbre vectors are best used in comparison with each other.
+        - Compute every basis mean, variance, median, min, and max value
+    """
     timbre = [i for i in zip(*data[number]["segments"]["timbre"])]
     for i in range(len(timbre)):
         tempt[f"Basis{i + 1}_mean"] = mean(timbre[i])
@@ -85,4 +129,4 @@ if __name__ == "__main__":
 
     new_df = pd.merge(df, feature, on="id")
 
-    # new_df.to_excel("CompleteData.xlsx", index=False, encoding="utf-8", header=True)
+    # new_df.to_excel("FILENAME.xlsx", index=False, encoding="utf-8", header=True)
